83de44b82cb99c6520e608e3bb0bc6fd019b4a7a
AC <anneclaire.fougerit@gmail.com>
2023-03-22 14:09:22 +0100

    ajout bouton scroll top

--- assets/JS/navbar.js
+++ assets/JS/navbar.js
@@ -1,5 +1,8 @@
+/* menu burger version mobile */
+
 const burger = document.querySelector(".menuBurger")
 const navLinks = document.querySelector(".navLinks")
+
 
 burger.addEventListener("click", function () {
     navLinks.classList.toggle("menu-mobile");
@@ -10,9 +13,6 @@ burger.addEventListener("click", function () {
 // - on se dirige directement vers la section souhaitée
 
 const menuMobile = document.getElementsByClassName(".navLinks.menu-mobile")
-
-// si je clique sur un li => navLinks se ferme (remove menu-mobile)
-
 const menuItems = document.querySelectorAll(".menuItems")
 
 function menuHidden() {
@@ -20,8 +20,22 @@ function menuHidden() {
     navLinks.classList.remove("menu-mobile")
 }
 
+/* au clique de chaque li, la classe s'enlève => le menu disparait*/
 menuItems.forEach(
     function (menuItems) {
         menuItems.addEventListener("click", menuHidden)
     }
 )
+
+/* boutton scroll top qui n'apparait qu'en fonction d'une valeur y du scroll vers le bas*/
+
+const hiddenBtn = document.getElementById(`hiddenButton`)
+
+window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 500) {
+        hiddenBtn.classList.add('scrollTopButton');
+    }else{
+        hiddenBtn.classList.remove('scrollTopButton');
+    }
+})
+
